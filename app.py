bcf00c12381ea08e79ccb95639703113a67c4aec
danhammer <dan.s.hammer@gmail.com>
2020-09-09 16:41:10 -0700

    fully loaded env initial commit

---
+++ app.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+import altair as alt
+import streamlit as st
+
+st.header("On delivering Appendix 1")
+st.subheader("An interactive outline")
+
+
+st.markdown("""
+
+> _Consistent and credible data are required to enable markets for ecosystem
+services. Recent advancements, even in the last few weeks, have enabled
+measurement at a scale and scope that is unprecedented. This web application
+shows the latest and greatest data for select areas in Costa Rica._  
+
+""")
+
+st.markdown("""
+
+	This roughcut web application is intended to illustrate both the baseline and
+	real-time environmental measurements on possible concessions. The front-end
+	is simple.  A fancy user interface or graphics can be built.  The objective
+	of this early application is to settle on the measurements that are most
+	useful for reporting and prioritization.  The data are not sufficient.  They
+	are not updated frequently enough and they are not published at high enough
+	spatial resolution to make economic decisions.  At the very least, this web
+	application can highlight the data gaps.
+
+""")
