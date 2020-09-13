3a05aa8d4e31d2e7d37898fc57bac1ada8023c67
danhammer <dan.s.hammer@gmail.com>
2020-09-13 09:44:39 -0700

    added soilgrids image and scatterplot

--- app.py
+++ app.py
@@ -15,10 +15,13 @@ st.markdown("""
 
 @st.cache(persist=True)
 def load_data(plot=True):
-	ocarbon = pd.read_pickle('data/ocarbon.pkl')
-	return ocarbon
+	data = {
+		"ocarbon": pd.read_pickle('data/ocarbon.pkl'),
+		"soilgrid_corr": pd.read_pickle('data/soilgrid_corr.pkl')
+	}
+	return data
 
-ocarbon = load_data()
+data = load_data()
 
 st.markdown("""
 
@@ -56,6 +59,7 @@ st.image(
 	use_column_width=True
 )
 
+ocarbon = data["ocarbon"]
 nrow, _ = ocarbon.shape
 lamb = ocarbon.orgc_value_avg.mean()
 
@@ -88,6 +92,73 @@ c = alt.Chart(df).mark_bar(
 )
 
 st.altair_chart(c, use_container_width=True)
+
+st.markdown("""
+
+	Now, consider the SoilGrids data.  A sample of the U.S., three square tiles
+	at 2 degrees sides is plotted below, where lighter colors indicate higher
+	levels of Organic Soil Carbon Stock. *Note that the image is squished due to
+	projection being totally ignored &mdash; the image is just for, literally,
+	color commentary.*   This area contains 4,095 WoSIS measurements.
+
+""")
+
+image = Image.open('imgs/soilgrids_sample.png')
+
+st.image(
+	image, 
+	caption='SoilGrids sample in the U.S. (six degrees across)',
+	use_column_width=True
+)
+
+st.markdown("""
+	
+	The SoilGrid data is a very rough approximation of the ground samples.  Much
+	more precise research and coordination has to be done (**Chris, help!!**) to
+	ensure that we're comparing apples to apples, but the scatterplot below shows
+	that the correlation is positive but weak.
+
+""")
+
+soilgrid_corr = data["soilgrid_corr"]
+
+base = alt.Chart(soilgrid_corr).mark_circle(
+		opacity=0.5,
+		color="#A9BEBE"
+	).encode(
+	    alt.X(
+	    	'wosis:Q',
+	    	scale=alt.Scale(domain=[0,90], clamp=True),
+	    	title="WoSIS measurement"
+	    ),
+	    alt.Y(
+	    	'soilgrid:Q',
+	    	title="SoilGrid pixel (250m) value"
+	    )
+)
+
+st.altair_chart(base, use_container_width=True)
+
+st.markdown("""
+
+	[SoilGrids addresses the uncertainty directly](https://www.isric.org/explore/soilgrids/faq-soilgrids-2017#How_accurate_are_SoilGrids_predictions):
+
+	> The actual mapping accuracy of each targeted soil property and classes is
+	still limited: the amount of variation explained by the models ranges between
+	30 and 70%. On the other hand, and in comparison to other previous global
+	soil databases, SoilGrids provides an objective estimate of the uncertainty
+	of mapping.
+
+	In other words, the global estimates are highly uncertain &mdash; maybe not
+	even useful &mdash; but at least the uncertainty is open and spatially
+	explicit.  Given that the accuracy of SoilGrids is at least as good as any
+	other global data source on soil organic carbon, and the precision is
+	explicit, a **value added would be to report the uncertainty translates for
+	agricultural land and how that uncertainty translates into trends.**
+
+""")
+
+
 
 st.markdown("""
 	#### [OpenLandMap](https://openlandmap.org/)
